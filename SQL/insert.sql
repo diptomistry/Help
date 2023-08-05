0b85d9454be89aca59804341640be019ace121c9
Diptajoy Mistry <111699492+diptomistry@users.noreply.github.com>
2023-08-05 17:11:23 +0600

    Create insert.sql

---
+++ SQL/insert.sql
@@ -0,0 +1,6 @@
+--inserting rows:
+INSERT INTO employees (first_name, last_name, hire_date)--(no need to use this if u want to insert into all columns)
+VALUES
+    ('Alice', 'Smith', '2023-02-01'),
+    ('Bob', 'Johnson', '2023-02-15'),
+    ('Emily', 'Brown', '2023-03-01');
