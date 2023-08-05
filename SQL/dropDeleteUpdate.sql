598ecf4d02a517f78e6a752c2d633679a8a84f66
Diptajoy Mistry <111699492+diptomistry@users.noreply.github.com>
2023-08-05 18:31:39 +0600

    Update and rename dropDelete.sql to dropDeleteUpdate.sql

--- SQL/dropDeleteUpdate.sql
+++ SQL/dropDeleteUpdate.sql
@@ -8,3 +8,20 @@ conditions. The "DELETE" command doesn't remove the table structure or the table
 */
 
 DELETE FROM table_name WHERE condition;
+--Update a single column for specific rows:
+UPDATE employees
+SET salary = salary * 1.10
+WHERE department = 'Sales';
+--Update multiple columns for specific rows:
+UPDATE products
+SET price = price * 0.90,
+    quantity_in_stock = quantity_in_stock - 1
+WHERE category = 'Electronics' AND price > 1000;
+--Update all rows:
+UPDATE customers
+SET status = 'Inactive';
+--Using subqueries to update data:
+UPDATE orders
+SET total_amount = (SELECT SUM(order_total) FROM order_items WHERE order_id = orders.id)
+WHERE order_date >= '2023-01-01';
+
