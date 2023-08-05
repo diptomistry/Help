e01815f66988a9d933b449ed29691d7215574b7f
Diptajoy Mistry <111699492+diptomistry@users.noreply.github.com>
2023-08-05 13:53:49 +0600

    Update schema.sql

--- SQL/schema.sql
+++ SQL/schema.sql
@@ -1,3 +1,5 @@
-In MySQL, the term "default schema" refers to the database that is automatically selected and used when you connect to the MySQL server.
+/*In MySQL, the term "default schema" refers to the database that is automatically selected and used when you connect to the MySQL server.
 When you issue SQL queries without explicitly specifying a database name, the default schema is where MySQL will assume you want to perform
-the operations.Primarily sys is the default schema
+the operations.Primarily sys is the default schema*/
+to change default schema:
+use datebaseName;
