03d29d8bf87198fd28fd9eeb70df439a63bd0159
Diptajoy Mistry <111699492+diptomistry@users.noreply.github.com>
2023-08-05 13:47:53 +0600

    Create schema.sql

---
+++ SQL/schema.sql
@@ -0,0 +1,3 @@
+In MySQL, the term "default schema" refers to the database that is automatically selected and used when you connect to the MySQL server.
+When you issue SQL queries without explicitly specifying a database name, the default schema is where MySQL will assume you want to perform
+the operations.Primarily sys is the default schema
