b10f3b111fbe7cd197a2694940279c7b0fa7d2b9
Diptajoy Mistry <111699492+diptomistry@users.noreply.github.com>
2023-08-05 14:26:57 +0600

    Create select.sql

---
+++ SQL/select.sql
@@ -0,0 +1,24 @@
+SELECT * FROM employees;--selects all columns
+--to select specific column:
+SELECT first_name, last_name
+FROM employees
+WHERE department = 'Sales';
+--select with multiple conditions:
+SELECT product_name, price
+FROM products
+WHERE category = 'Electronics' AND price > 500;
+--select with sorting:
+SELECT customer_name, order_date, total_amount
+FROM orders
+WHERE order_date >= '2023-01-01'
+ORDER BY order_date DESC;
+--Select with aggregation (using functions):
+SELECT department, AVG(salary) AS avg_salary, COUNT(*) AS num_employees
+FROM employees
+GROUP BY department;
+--select with joining tables:
+SELECT customers.customer_name, orders.order_date, orders.total_amount
+FROM customers
+INNER JOIN orders ON customers.customer_id = orders.customer_id;
+
+
