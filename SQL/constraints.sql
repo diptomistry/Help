7613e968e0d794c67f829f35fa2fcac377a01bb0
Diptajoy Mistry <111699492+diptomistry@users.noreply.github.com>
2023-08-06 22:10:19 +0600

    Update constraints.sql

--- SQL/constraints.sql
+++ SQL/constraints.sql
@@ -1,9 +1,9 @@
-#UNIQUE:
+                            #UNIQUE#
 --Ensures that all values in a column (or a set of columns) are unique.
 --Allows NULL values, but no two non-NULL values can be the same.
 CREATE TABLE employees (
   employee_id INT PRIMARY KEY,
-  email VARCHAR(100) UNIQUE,--we cant insert any email that are the same//we can use NOT NULL ALSO
+  email VARCHAR(100) UNIQUE,--we cant insert any email that are the same//we can use NOT NULL also(email VARCHAR(100) NOT NULL)
   department VARCHAR(50)
 );
 --unique constraint to an existing table:
@@ -16,7 +16,7 @@ ALTER TABLE employees
 ADD CONSTRAINTS UNIQUE(email);--MODIFY email VARCHAR(100)  NOT NULL;
 
 
-#CHECK:
+                            #CHECK#
 --Specifies a condition that must be true for each row.
 --Prevents insertion or update of data that does not meet the specified condition.
 --Example: 
@@ -29,5 +29,9 @@ CREATE TABLE orders (
 --For existing table:
 ALTER TABLE orders
 ADD CONSTRAINTS chk_total_amount CHECK (total_amount > 0) ;
+--to remove:
+ALTER TABLE orders
+DROP CHECK chk_total_amount;
+
 
 
