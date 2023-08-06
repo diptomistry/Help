0d17624ceb6057e29cecb5df8e843ee93d7dbcfc
Diptajoy Mistry <111699492+diptomistry@users.noreply.github.com>
2023-08-06 21:21:36 +0600

    Update time.sql

--- SQL/time.sql
+++ SQL/time.sql
@@ -1,4 +1,4 @@
 SELECT CURRENT_DATE(); -- Returns 'YYYY-MM-DD' format, e.g., '2023-08-05'
 SELECT CURRENT_TIME(); -- Returns 'HH:MM:SS' format, e.g., '14:30:45'
-INSERT INTO events (event_name, event_date, event_time)
-VALUES ('Conference', CURRENT_DATE(), CURRENT_TIME());
+INSERT INTO events (event_name, event_date, event_time,event_dateTime)
+VALUES ('Conference', CURRENT_DATE(), CURRENT_TIME(),NOW());--for tomorrow CURRENT_DATE()+1
