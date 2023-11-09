b83c9708e384122ce50169e9e912877d0073799d
Diptajoy Mistry <111699492+diptomistry@users.noreply.github.com>
2023-11-09 19:50:37 +0600

    Create 34_diptoJoy_Lab_4_5.sql

---
+++ SQL/oracle/34_diptoJoy_Lab_4_5.sql
@@ -0,0 +1,125 @@
+CREATE TABLE person (
+    id NUMBER PRIMARY KEY,
+    name VARCHAR2(255) NOT NULL,
+    dob DATE,
+    gender VARCHAR2(10) CHECK (gender IN ('Male', 'Female', 'Other')),
+    father_id NUMBER,
+    mother_id NUMBER,
+    CONSTRAINT fk_father FOREIGN KEY (father_id) REFERENCES person(id),
+    CONSTRAINT fk_mother FOREIGN KEY (mother_id) REFERENCES person(id)-- (i) The father and mother refer to the person_id, 
+);
+CREATE TABLE company (
+    id INT PRIMARY KEY,
+    name VARCHAR2(255) UNIQUE,
+    capital DECIMAL(18, 2)
+);
+CREATE TABLE investment (
+    company_id INT,
+    person_id INT,
+    comShare DECIMAL(10, 2),
+    FOREIGN KEY (company_id) REFERENCES company(id),
+    FOREIGN KEY (person_id) REFERENCES person(id)
+);
+                                       --insert into person--
+
+-- Male person with no parents
+INSERT INTO person (id, name, dob, gender, father_id, mother_id)
+VALUES (1, 'Mohammad Rahman', TO_DATE('1985-03-10', 'YYYY-MM-DD'), 'Male', NULL, NULL);
+
+-- Female person with parents
+INSERT INTO person (id, name, dob, gender, father_id, mother_id)
+VALUES (2, 'Fatima Ahmed', TO_DATE('1990-06-25', 'YYYY-MM-DD'), 'Female', 1, 1);
+
+-- Male person with parents
+INSERT INTO person (id, name, dob, gender, father_id, mother_id)
+VALUES (3, 'Abdul Khan', TO_DATE('1980-12-05', 'YYYY-MM-DD'), 'Male', 1, 2);
+
+-- Female person with parents
+INSERT INTO person (id, name, dob, gender, father_id, mother_id)
+VALUES (4, 'Ayesha Rahman', TO_DATE('1995-09-15', 'YYYY-MM-DD'), 'Female', 3, 2);
+-- Inserting more Bangladeshi data into the person table
+
+-- Male person with parents
+INSERT INTO person (id, name, dob, gender, father_id, mother_id)
+VALUES (5, 'Arif Khan', TO_DATE('1992-08-20', 'YYYY-MM-DD'), 'Male', 3, 4);
+
+-- Female person with parents
+INSERT INTO person (id, name, dob, gender, father_id, mother_id)
+VALUES (6, 'Nadia Ahmed', TO_DATE('1998-04-12', 'YYYY-MM-DD'), 'Female', 1, 2);
+
+-- Male person with no parents
+INSERT INTO person (id, name, dob, gender, father_id, mother_id)
+VALUES (7, 'Rahim Hasan', TO_DATE('1987-11-30', 'YYYY-MM-DD'), 'Male', NULL, NULL);
+
+-- Female person with parents
+INSERT INTO person (id, name, dob, gender, father_id, mother_id)
+VALUES (8, 'Sabina Khan', TO_DATE('1994-02-18', 'YYYY-MM-DD'), 'Female', 5, 6);
+
+-- Male person with parents
+INSERT INTO person (id, name, dob, gender, father_id, mother_id)
+VALUES (9, 'Imran Ahmed', TO_DATE('1983-06-08', 'YYYY-MM-DD'), 'Male', 7, 8);
+
+-- Female person with parents
+INSERT INTO person (id, name, dob, gender, father_id, mother_id)
+VALUES (10, 'Farida Rahman', TO_DATE('1996-09-22', 'YYYY-MM-DD'), 'Female', 9, 8);
+
+-- Male person with parents
+INSERT INTO person (id, name, dob, gender, father_id, mother_id)
+VALUES (11, 'Samiul Islam', TO_DATE('1989-12-15', 'YYYY-MM-DD'), 'Male', 7, 6);
+
+-- Female person with parents
+INSERT INTO person (id, name, dob, gender, father_id, mother_id)
+VALUES (12, 'Nazia Khan', TO_DATE('1991-03-28', 'YYYY-MM-DD'), 'Female', 5, 4);
+
+-- Male person with parents
+INSERT INTO person (id, name, dob, gender, father_id, mother_id)
+VALUES (13, 'Ashraf Ahmed', TO_DATE('1980-05-10', 'YYYY-MM-DD'), 'Male', 11, 12);
+
+-- Female person with parents
+INSERT INTO person (id, name, dob, gender, father_id, mother_id)
+VALUES (14, 'Sultana Rahman', TO_DATE('1985-10-05', 'YYYY-MM-DD'), 'Female', 13, 12);
+INSERT INTO person (id, name, dob, gender, father_id, mother_id)
+VALUES (15, 'Fariha Haque', TO_DATE('1985-10-05', 'YYYY-MM-DD'), 'Female', NULL, 12);
+INSERT INTO person (id, name, dob, gender, father_id, mother_id)
+VALUES (16, 'Rakib Hassan', TO_DATE('1995-10-05', 'YYYY-MM-DD'), 'Female', 12, NULL);
+INSERT INTO person (id, name, dob, gender, father_id, mother_id)
+VALUES (17, 'Rakib Khan', TO_DATE('2010-10-05', 'YYYY-MM-DD'), 'Male', 11, 10);
+
+                                    --insert into company--
+INSERT INTO company (id, name, capital) VALUES (1, 'Google', 500000.00);
+INSERT INTO company (id, name, capital) VALUES (2, 'Microsoft', 750000.50);
+INSERT INTO company (id, name, capital) VALUES (3, 'Oracle', 300000.75);
+
+                                 --insert into investment--
+INSERT INTO investment (company_id, company_name, person_id, comShare)
+VALUES (1, 'Google', 1, 20.50);
+
+INSERT INTO investment (company_id, company_name, person_id, comShare)
+VALUES (2, 'Microsoft', 3, 15.75);
+
+INSERT INTO investment (company_id, company_name, person_id, comShare)
+VALUES (3, 'Google', 5, 30.25);
+
+INSERT INTO investment (company_id, company_name, person_id, comShare)
+VALUES (4, 'Microsoft', 2, 25.00);
+
+INSERT INTO investment (company_id, company_name, person_id, comShare)
+VALUES (5, 'Google', 4, 10.50);
+
+INSERT INTO investment (company_id, company_name, person_id, comShare)
+VALUES (6, 'Microsoft', 6, 40.75);
+
+INSERT INTO investment (company_id, company_name, person_id, comShare)
+VALUES (7, 'Google', 8, 18.20);
+
+INSERT INTO investment (company_id, company_name, person_id, comShare)
+VALUES (8, 'Microsoft', 10, 22.80);
+
+INSERT INTO investment (company_id, company_name, person_id, comShare)
+VALUES (9, 'Google', 7, 35.60);
+
+INSERT INTO investment (company_id, company_name, person_id, comShare)
+VALUES (10, 'Oracle', 9, 28.90);
+
+
+
